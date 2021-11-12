79b491c7bbed716700af9f19f9237bf04efbbbb2
AGEM20 <alberto.rodrigueslopes@gmail.com>
2021-11-11 22:12:40 -0300

    Initial commit

---
+++ cities-pi/src/test/java/com/githuib/AGEM20/citiespi/CitiesPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githuib.AGEM20.citiespi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/githuib/AGEM20/citiespi/CitiesPiApplication.java
@@ -0,0 +1,13 @@
+package com.githuib.AGEM20.citiespi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesPiApplication.class, args);
+	}
+
+}
